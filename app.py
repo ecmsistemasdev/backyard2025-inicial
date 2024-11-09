a35d2fd6603217e9024c3e4ad29df093d158ffe0
ecmsistemasdev <164636970+ecmsistemasdev@users.noreply.github.com>
2024-11-09 13:10:46 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -9,25 +9,30 @@ st.markdown("### Macaxeira Backyard Ultra - 2ª Edição")
 st.write("")
 #st.markdown("### 🔒Inscrições Encerradas")
 
-st.link_button(label="Clique aqui para realizar sua inscrição",url="https://macaxeirabackyard-inscricao-6ae3f0f0b354.herokuapp.com/",type="primary")
+st.link_button(label="Clique aqui para realizar sua inscrição",url="https://backyard-inscricao-d47b401cc76a.herokuapp.com/",type="primary")
 
 with st.form("Informativo", border=False):
     st.markdown("##### Informativo da Corrida")
-    st.write("📅 Dia 22 de fevereiro de 2025")
-    st.write("🏃🏻 Largada às 08hs 🕗")
-    #st.write("📍 Saída: Início do Espaço Alternativo - Av. Jorge Teixeira (Praia)")
-    st.caption("")
-    st.write("💲 Valor da Inscrição: 279,00 reais para prova Principal; 179,00 para prova de três voltas")
-    st.write("💲 Atleta com 60 ou mais, paga 50% do valor integral")
+    with open('Informativo.txt', 'r', encoding='UTF-8') as f:
+        lines = f.readlines()
+        for line in lines:
+            st.write(line)
+
+    #st.write("📅 Dia 22 de fevereiro de 2025")
+    #st.write("🏃🏻 Largada às 08hs 🕗")
+    #st.caption("")
+    #st.write("💲 Valor da Inscrição: 279,00 reais para prova Principal; 179,00 para prova de três voltas")
+    #st.write("💲 Atleta com 60 ou mais, paga 50% do valor integral")
     
-    st.write("Forma de Pagamento: ")
-    st.write("  Pix kelioesteves@hotmail.com - Kélio Esteves Xavier - Mercado pago.")
-    st.write("📱 Mais informações: (69) 99925-9005")
-    st.caption("")
-    st.write("INSCRIÇÕES:")
-    st.write("✍️ Período de inscrição:")
-    st.write("  Início: 10 de novembro de 2024")
-    st.write("  Término: 06 de janeiro 2025 ou até o limite das vagas")
+    #st.write("Forma de Pagamento: ")
+    #st.write("  Pix kelioesteves@hotmail.com - Kélio Esteves Xavier - Mercado pago.")
+    #st.write("📱 Mais informações: (69) 99925-9005")
+    #st.caption("")
+    #st.write("INSCRIÇÕES:")
+    #st.write("✍️ Período de inscrição:")
+    #st.write("  Início: 10 de novembro de 2024")
+    #st.write("  Término: 06 de janeiro 2025 ou até o limite das vagas")
+    
     st.form_submit_button("",disabled=True)
 
 with st.form("Regulamento"):
@@ -36,7 +41,8 @@ with st.form("Regulamento"):
         lines = f.readlines()
         for line in lines:
             st.write(line)
-    st.link_button(label="Clique aqui para realizar sua inscrição",url="https://macaxeirabackyard-inscricao-6ae3f0f0b354.herokuapp.com/",type="primary")
+    st.link_button(label="Clique aqui para realizar sua inscrição",url="https://backyard-inscricao-d47b401cc76a.herokuapp.com/",type="primary")
+    #st.link_button(label="Clique aqui para realizar sua inscrição",url="http://191.217.246.233:8501/",type="primary")
     st.form_submit_button("",disabled=True)
 
 from PIL import Image
